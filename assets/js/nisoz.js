1daa42c2e7038db2d9bf31b23a148f0a13b05740
B-Cris <cristian.baciu@creative-pioneers.dev>
2024-05-12 21:52:37 +0300

    solve js problem

--- assets/js/nisoz.js
+++ assets/js/nisoz.js
@@ -110,7 +110,7 @@
 
     selector.find("li").each(function () {
       let anchor = $(this).find("a");
-      if ($(anchor).attr("href") == FileName) {
+      if ($(anchor).attr("href").split("/")[-1] == FileName) {
         $(this).addClass("current");
       }
     });
