60120cdd27e0f4de6f0d165be2784ac25bccd753
Martin Hrášek <martin.hrasek@drmax.eu>
2024-07-26 00:47:45 +0200

    Add JS compilation and minification

--- resources/script.ts
+++ resources/script.ts
@@ -1,6 +1,6 @@
-require('./js/jquery.js')
-require('./js/jquery.smartmenus.min.js')
-require('./js/isotope.pkgd.js')
-require('./js/jquery.prettyPhoto.js')
-require('./js/swiper.min.js')
-require('./js/main.js')
+import './js/jquery.js'
+import './js/jquery.smartmenus.min.js'
+import './js/isotope.pkgd.js'
+//require('./js/jquery.prettyPhoto.js')
+import './js/swiper.min.js'
+import './js/main.js'
